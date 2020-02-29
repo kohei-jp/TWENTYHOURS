97e5f486d46a7d649045a9c96c5cd64ced7f3c56
kohei-jp <kohei.mitateda@gmail.com>
2020-02-29 13:46:55 +0900

    api

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -2,4 +2,22 @@ $("#menu li").hover(function() {
   $(this).children('ul').show();
 }, function() {
   $(this).children('ul').hide();
+<<<<<<< Updated upstream
 });
+=======
+});
+
+
+$(function(){
+  var $ppc = $('.progress-pie-chart'),
+    percent = parseInt($ppc.data('percent')),
+    deg = 360 * percent / 100;
+  if (percent > 50) {
+    $ppc.addClass('gt-50');
+  }
+  $('.ppc-progress-fill').css('transform','rotate('+ deg +'deg)');
+  $('.ppc-percents span').html(percent+'%');
+});
+
+
+>>>>>>> Stashed changes
